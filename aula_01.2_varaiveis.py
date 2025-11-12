1293f73fb3f1cc0e9bd8d071195cf452b730f43f
Marcelo Silva <mhps.study@gmail.com>
2025-11-12 19:07:49 -0300

    feat: aprendendo sobre variáveis

---
+++ aula_01.2_varaiveis.py
@@ -0,0 +1,24 @@
+# Aprendendo sobre variáveis
+# Variáveis são símbolos ou nomes que representa um valor.
+
+#%%
+nome = "Marcelo Silva" # a variável nome está tendo o valor "Marcelo Silva" sendo atribuída a ela
+print(nome)
+
+# A variável nome pode ser reatribuída, mudando seu valor novamente.
+nome = "Garrafa de Água"
+print(nome)
+
+#%%
+soma = 1+1 # Outro caso de atribuição usando números
+print("1 + 1 = ", soma)
+
+#%%
+resultado = 10 - 5
+print(resultado)
+
+resultado = 10 * 2
+print(resultado)
+
+resultado = resultado + 10
+print(resultado)
