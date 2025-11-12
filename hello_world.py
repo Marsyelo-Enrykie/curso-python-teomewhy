e45199882fbb58bd891c90a7a59641c81c1be984
Marcelo Silva <mhps.study@gmail.com>
2025-11-12 18:23:11 -0300

    feat: add modo interativo

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,7 @@
 print("Hello World!")
 
 print("Iniciando curso de Python do canal Téo Me Why!")
+
+# %%
+# Aprendendo a usar o modo interativo do Jupyter Notebook
+print("Este é um exemplo de saída de modo interativo")
