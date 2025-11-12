53839adf61948ff6da32067ca472b5063982f97c
Marcelo Silva <mhps.study@gmail.com>
2025-11-12 18:36:36 -0300

    Feat: aprendendo sobre operações aritméticas

---
+++ aula_01_calculadora.py
@@ -0,0 +1,29 @@
+# Aprendendo sobre operações aritméticas
+
+#%%
+# SOMA
+print("1 + 1 = ", 1 + 1)
+
+#%%
+# SUBTRAÇÃO
+print("10 - 5 = ", 10 - 5)
+
+#%%
+# MULTIPLICAÇÃO
+print("5 * 2 = ", 5 * 2)
+
+
+#%%
+# DIVISÃO
+# 1. DIVISÃO NORMAL
+print("10 / 5 = ", 10 / 5)
+
+# 2. DIVISÃO DE PARTE INTEIRA
+print("10 / 3 = ", 10 // 3)
+
+# 3. RESTO DA DIVISÃO
+print("10 / 4 = ", 10 % 4)
+
+#%%
+# POTÊNCIA
+print("3² = ", 3 ** 2)
